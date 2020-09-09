15ba9a53d9fc3c5595f08bec4ffdb4696ff7fb6d
Denis Sannikov <smaylik911@gmail.com>
2020-09-09 21:26:20 +0700

    finish layouts

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const path = require('path')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
 
-const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
 const jsLoaders = () => {
   const loaders = [
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env'],
-      },
-    },
+        presets: ['@babel/preset-env']
+      }
+    }
   ]
+
   if (isDev) {
     loaders.push('eslint-loader')
   }
+
   return loaders
 }
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: fileName('js'),
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -44,19 +47,23 @@ module.exports = {
     hot: isDev
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
     }),
-    new CopyPlugin({
-      patterns: [{
+    new CopyPlugin([
+      {
         from: path.resolve(__dirname, 'src/favicon.ico'),
         to: path.resolve(__dirname, 'dist')
-      }]
-    }),
-    new CleanWebpackPlugin(),
+      }
+    ]),
     new MiniCssExtractPlugin({
-      filename: fileName('css')
-    }),
+      filename: filename('css')
+    })
   ],
   module: {
     rules: [
@@ -71,14 +78,14 @@ module.exports = {
             }
           },
           'css-loader',
-          'sass-loader',
+          'sass-loader'
         ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: jsLoaders()
-      },
-    ],
+      }
+    ]
   }
 }
